8722e33fa759a09839c6d749cd62d2590601dd9c
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-07-02 12:19:48 -0300

    Página home

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+/* Start Initial libry Scroll */
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['horttas', 'o-que-e-horttas' ,'alimentos-organicos', 'faca-parte', 'formulario'],
+    menu: '#menu',
+    css3: true
+});
