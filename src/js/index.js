80e45b9ab8bbba288c51eb25570987d8aa98cc93
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-07-06 12:37:17 -0300

    Ajuste nas seções - Mobile e DESKTOP

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 /* Start Initial libry Scroll */
 var myFullpage = new fullpage('#fullpage', {
 
-    anchors: ['horttas', 'o-que-e-horttas' ,'alimentos-organicos', 'faca-parte', 'formulario'],
-    menu: '#menu',    
+    anchors: ['horttas', 'o-que-e-horttas', 'alimentos-organicos', 'faca-parte', 'formulario'],
+    menu: '#menu',
     responsiveWidth: 1024,
     css3: true
 });
+
+
+$('#whatsapp').mask('(00) 9 0000-0000');
+
